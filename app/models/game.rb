312558f26967ae558c3f65cd412d9fe1c2486e86
Matthew Brand <mattbrand@gmail.com>
2016-05-02 10:44:56 -0400

    annotations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: games
+#
+#  id         :integer          not null, primary key
+#  winner     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Game < ActiveRecord::Base
   validates_presence_of :winner, unless: :new_record?
 end
