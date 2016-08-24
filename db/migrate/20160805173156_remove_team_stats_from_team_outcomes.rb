50d57d8354917040e5eb44342e2bf4f3cae52a47
Alex Chaffee <alex@stinky.com>
2016-08-24 07:38:28 -0700

    i never asked for this migration

--- db/migrate/20160805173156_remove_team_stats_from_team_outcomes.rb
+++
@@ -1,7 +0,0 @@
-class RemoveTeamStatsFromTeamOutcomes < ActiveRecord::Migration
-  def change
-    remove_column :outcomes, :team_stats_id, :integer
-  rescue PG::UndefinedColumn, ActiveRecord::StatementInvalid
-    # dunno how this column got out of sync
-  end
-end
