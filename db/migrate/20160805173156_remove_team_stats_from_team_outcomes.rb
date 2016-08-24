e361bf91f505b7e56e987a31f51a49017ed530c7
Alex Chaffee <alex@stinky.com>
2016-08-24 07:33:18 -0700

    clean up migration glitch

--- db/migrate/20160805173156_remove_team_stats_from_team_outcomes.rb
+++ db/migrate/20160805173156_remove_team_stats_from_team_outcomes.rb
@@ -1,5 +1,7 @@
 class RemoveTeamStatsFromTeamOutcomes < ActiveRecord::Migration
   def change
-    remove_column :team_outcomes, :team_stats_id, :integer
+    remove_column :outcomes, :team_stats_id, :integer
+  rescue PG::UndefinedColumn
+    # dunno how this column got out of sync
   end
 end
