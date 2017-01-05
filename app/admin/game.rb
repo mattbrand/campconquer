121cf95f6b3fa4500b170dfd4e22952171769c19
Alex Chaffee <alex@stinky.com>
2017-01-05 14:47:42 -0500

    allow scheduled_start to be edited

--- app/admin/game.rb
+++ app/admin/game.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Game do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :state
+  permit_params :state, :scheduled_start
 
   filter :created_at
   filter :updated_at
@@ -15,7 +15,8 @@ ActiveAdmin.register Game do
     column :updated_at
     column :state
     # column :pieces  # todo: show pieces for this game only -- how? 
-
+    column :scheduled_start
+    # column :played_at
     actions
   end
 
